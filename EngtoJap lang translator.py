9e4f08c285f78e3e66e04bf465adcde8dd36ca15
Vipin Yadav <vipinyadav546@gmail.com>
2020-04-20 00:44:04 +0530

    hi this is a language translator tool for eng to jap

---
+++ EngtoJap lang translator.py
@@ -0,0 +1,13 @@
+from translate  import Translator
+translator= Translator(to_lang="zh")
+try:
+	with open("test.txt",mode='r') as my_file:
+		text=my_file.read()
+		translation = translator.translate(text)
+		with open("test_zh",mode='w') as my_file2:
+			my_file2.write(translation)
+except FileNotFoundError as e:
+	print("check your file path")
+
+
+	
